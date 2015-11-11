5d5b51e0cbd965aedc6384d48eb55a7cbe80eeba
Atle Haugan <atleh@statoil.com>
2015-11-11 22:47:36 +0100

    Updated hatch text color

--- js/script.js
+++ js/script.js
@@ -7,20 +7,31 @@ $(document).ready(function() {
         .append("g")
         .attr("transform", "translate(0, " + yTransform + ")");
     for (col = 1; col <= colsPerRow; col++) {
+      var hatchNo = (row-1) * colsPerRow + col;
       var xTransform = 50 + (col - 1) * 150;
       cell = rowGroup.append("g")
-          .attr("transform", "translate(" + xTransform + ",0)");
+          .attr("transform", "translate(" + xTransform + ",0)")
+          .on("click", function() {
+            var element = d3.select(this);
+            console.log("clicked: ", element);
+          });
+      cell.append("text")
+          .attr("id", hatchNo)
+          .attr("class", "hatch")
+          .attr("x", "28")
+          .attr("y", "48")
+          .text(hatchNo);
       cell.append("rect")
           .attr("class", "hatch")
           .attr("x", 0)
           .attr("y", 0)
           .attr("height", "80")
           .style("width", "100");
-      cell.append("text")
-          .attr("class", "hatch")
-          .attr("x", "28")
-          .attr("y", "48")
-          .text(row*col);
     }
   }
 });
+
+function hatchOpener(d, i) {
+  var hatchElement = d3.select(this);
+  console.log("Opening hatch: " + this.id);
+}
